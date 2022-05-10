d010467a9cf1261008a6c199fed65c8b073c11ac
mrtom222 <38867467+mrtom222@users.noreply.github.com>
2022-05-10 11:35:54 +0000

    Commit5_2

--- script.js
+++ script.js
@@ -35,6 +35,22 @@
 
   cw2.addEventListener("click", function () {
     //TODO implement it
+    answer.innerHTML = "<h1>Loading...</h1>";
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(array => {
+        //console.log(array[array.length-1].id)
+        console.log(array.length);
+        var output = "";
+        for(var i = 0 ;i<array.length;i++){
+          console.log(i);
+          output += "</br>" + array[i].id +/* "/" + array.lenght + */"</br>";
+          output += "<h1>" + array[i].title + "</h1></br>";
+          output += "<h2>" + array[i].body.replace("\n","</br>") + "</h2></br>";
+        }
+        console.log(output);
+        answer.innerHTML = output;
+      })
   })
 
   cw3.addEventListener("click", function () {
